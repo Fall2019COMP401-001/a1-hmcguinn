4186a19bfb2e3007c7e5535d991805e14dbbeb29
houlton <houlton@houlton-pc>
2019-08-28 22:26:13 -0400

    Finished A1 Novice

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -7,41 +7,43 @@ public class A1Novice {
 	public static void main(String[] args) {
 
 		Scanner scan = new Scanner(System.in);
-		double costOfItems = 0.0;
-
 
 		int totalCustomers = scan.nextInt(); 
 
+		// FOR loop that iterates once for each customer in input-set
+		
 		for (int i = 0; i <= totalCustomers; i++)	 {	
 
-			/* 
-			 * firstName: First name of customer
-			 * lastName: Last name of customer
-			 * itemsBought: Number of items bought by customer
-			 */
-			
+				/* 
+				 * firstName: First name of customer
+				 * lastName: Last name of customer
+				 * itemsBought: Number of items bought by customer
+				 * costOfItems: Variable used to print total cost for one customer
+				 */
+				double costOfItems = 0.0;
+
 				String firstName = scan.next(); 
 				String lastName  = scan.next();
 				int itemsBought = scan.nextInt(); 
 				
-				// For loop calculates cost of Items for a single customer
+					// For loop calculates cost of items for a single customer
+				
 					for (int ii = 0; ii < itemsBought; ii++) {
 					
-							int quantity = scan.nextInt(); 
-							String trash = scan.next(); 
-							double priceOfItem = scan.nextDouble(); 
+								int quantity = scan.nextInt(); 
+								String trash = scan.next(); 
+								double priceOfItem = scan.nextDouble(); 
+
+								costOfItems = costOfItems + (quantity * priceOfItem); 
+							}
 					
-							costOfItems = costOfItems + (quantity * priceOfItem); 
-						}
+				// Prints out total cost per customer in correct format 
 					
-				String customerTemp = (firstName.charAt(0) + ". " + lastName + 
-									 ": " + costOfItems );
-				
-				
-				System.out.println(customerTemp);
-		}
-				
-		
+					System.out.println(firstName.charAt(0) + ". " + lastName + 
+							": " + costOfItems );
+	
+
 
 	}
+}
 }
