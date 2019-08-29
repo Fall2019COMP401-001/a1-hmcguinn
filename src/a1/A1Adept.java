d153e4f91e5057ce75c5eaf75c938ade1059edfe
houlton <houlton@houlton-pc>
2019-08-28 23:09:53 -0400

    working on A1 adept

--- src/a1/A1Adept.java
+++ src/a1/A1Adept.java
@@ -7,8 +7,32 @@ public class A1Adept {
 	public static void main(String[] args) {
 		
 		Scanner scan = new Scanner(System.in);
-
-		// Your code follows here.
 		
+		//CLASS FOR EACH CUSTOMER
+		
+		int totalItems = scan.nextInt(); 
+
+		
+		// FOR loop that initalizes an object for each item 
+		
+		double[] itemCost = new double[totalItems];
+		String[] itemName = new String[totalItems];
+		
+		for(int i = 0; i < totalItems; i++) {
+			itemName[i] = scan.next();
+			itemCost[i] = scan.nextDouble(); 
+		}
+		
+		int totalCustomers = scan.nextInt(); 
+		
+		String[] firstName = new String[totalCustomers];
+		String[] lastName = new String[totalCustomers];
+		String[] itemsBought = new String[totalCustomers];
+
+		
+		
+		
+		
+		}
 	}
-}
+
