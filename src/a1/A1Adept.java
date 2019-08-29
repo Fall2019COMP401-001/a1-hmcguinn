55236de51e9c5eb84845a547623522092f04d233
houlton <houlton@houlton-pc>
2019-08-28 23:41:05 -0400

    working on exception thrown on line 39

--- src/a1/A1Adept.java
+++ src/a1/A1Adept.java
@@ -28,8 +28,74 @@ public class A1Adept {
 		String[] firstName = new String[totalCustomers];
 		String[] lastName = new String[totalCustomers];
 		String[] itemsBought = new String[totalCustomers];
-
+		double[] totalCost = new double[totalCustomers]; 
 		
+		for(int ii = 0; ii < totalCustomers; ii++) { 
+			firstName[ii] = scan.next(); 
+			lastName[ii] = scan.next(); 
+			
+			int numberOfItems = scan.nextInt(); 
+			
+			int tempQuantity = scan.nextInt(); 
+			String tempName = scan.next(); 
+			double tempCost = 0.0; 
+			double costForCustomer = 0.0;
+				for(int iii = 0; iii < totalItems; iii++) {
+					if(tempName.equals(itemName[iii])) {
+						costForCustomer = costForCustomer + (tempQuantity * tempCost); 
+					}
+					totalCost[ii] = costForCustomer;
+				}
+			
+			
+			
+		}
+	
+		// CALCULATE AVERAGE COST PER CUSTOMER
+		double averageCost = 0.0; 
+		
+		for(int x = 0; x < totalCustomers; x++) {
+			double sumOfCosts = 0.0;
+			
+			sumOfCosts = sumOfCosts + totalCost[x]; 
+		}
+		
+		averageCost = averageCost / totalCustomers; 
+		
+		// CALCULATE MAX COST 
+		
+		int maxCostIndex = -1;
+		double maxCost = 0.0;
+		
+		for (int z = 0; z < totalCustomers; z++) {
+			if (totalCost[z] > maxCost)
+				maxCost = totalCost[z];
+				maxCostIndex = z; 
+		}
+		
+		// CALCULATE SMALLEST COST
+		
+		int smallestCostIndex = -1;
+		double smallestCost = 1000000;
+		
+		for (int w= 0; w < totalCustomers; w++) {
+			if (totalCost[w] < smallestCost)
+				smallestCost = totalCost[w];
+				smallestCostIndex = w; 
+		}
+		
+		
+		// Print the proper output 
+		
+		System.out.println("Biggest: " + firstName[maxCostIndex] + " " + lastName[maxCostIndex] + 
+						"(" + maxCost + ")"); 
+		
+		System.out.println("Smallest: " + firstName[smallestCostIndex] + " " + lastName[smallestCostIndex] + 
+				"(" + smallestCost + ")"); 
+		
+		System.out.println("Average: " + averageCost ); 
+
+
 		
 		
 		
