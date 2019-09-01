78842edd566d93417e17b73baec88b4262721eb2
houlton <houlton@houlton-pc>
2019-09-01 15:52:27 -0400

    saving before break

--- src/a1/A1Jedi.java
+++ src/a1/A1Jedi.java
@@ -8,7 +8,50 @@ public class A1Jedi {
 		
 		Scanner scan = new Scanner(System.in);
 
-		// Your code follows here.
+		
+		int totalItems = scan.nextInt(); 
+		
+		String[] itemNames = new String[totalItems]; 
+		int[][] itemsBought = new int[2][totalItems]; 
+		
+		for(int i = 0; i< totalItems; i++) {
+			itemNames[i] = scan.next(); 
+			double trash = scan.nextDouble(); 
+		}
+		
+		 int  totalCustomers = scan.nextInt();
+
+		
+
+			for(int i = 0; i< totalCustomers; i++) {
+				String trashName = scan.next();
+				trashName = scan.next(); 
+				
+				int numberOfItemsBought = scan.nextInt(); 
+				
+				for(int ii = 0; ii <numberOfItemsBought; ii++) {
+					int temp = scan.nextInt(); 
+					String tempName = scan.next(); 
+					int index = 0;
+					
+						for(int iii = 0; iii < totalItems; iii++) {
+							if(tempName == itemNames[iii]) {
+								itemsBought[0][iii]++; 
+							}
+						}
+						
+				}
+			
+
+			}
+		
+		
+		
+		
+		
+		
+		
+		
 		
 	}
 }
